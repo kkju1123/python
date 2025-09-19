ab10c8dad0ce8d54ed3cc600d0760cef809a54ec
keke <kkju1123@outlook.com>
2025-09-19 16:53:20 +0200

    network

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
 print("hellow")
+import numpy as np # import Numpy library to generate 
+
+weights = np.around(np.random.uniform(size=6), decimals=2) # initialize the weights
+biases = np.around(np.random.uniform(size=3), decimals=2) # initialize the biases
